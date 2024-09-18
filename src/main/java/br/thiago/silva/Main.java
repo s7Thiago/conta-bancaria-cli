e2cd7f1ac60104a8a6a61ff421d4f48927c9c5be
thiago.silva <thyagosousasilva@gmail.com>
2024-09-18 15:25:42 -0300

    initial commit

---
+++ src/main/java/br/thiago/silva/Main.java
@@ -0,0 +1,12 @@
+package br.thiago.silva;
+
+import br.thiago.silva.models.Login;
+import br.thiago.silva.models.Programa;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Programa.executar();
+        System.out.println("\nPrograma finalizado!\n\n");
+    }
+}
---
+++ src/main/java/br/thiago/silva/models/Titular.java
@@ -0,0 +1,14 @@
+package br.thiago.silva.models;
+
+public class Titular {
+    public String nome;
+    public String cpf;
+    public String senha;
+
+    // construtor
+    public Titular(String nome, String cpf, String senha) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.senha = senha;
+    }
+}
